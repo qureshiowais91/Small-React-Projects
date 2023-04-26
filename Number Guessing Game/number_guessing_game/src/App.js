4757d7220b9481bd77526933751baca33c27de2b
qureshiowais <qureshiowais91@gmail.com>
2023-04-26 13:28:00 +0530

    init

---
+++ Number Guessing Game/number_guessing_game/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import Guessnumber from "./components/Guessnumber";
+
+
+function App() {
+  const [flag,setFlag] = useState(false);
+  function getNumber(value) {
+    if(value!=0){
+      setFlag("not in range")
+    }
+    else if(value === Math.random()*10 ){
+      setFlag(true)
+    }else{
+      setFlag(false)
+    }
+  }
+  return (
+    <div className="App">
+      <Guessnumber enteredNumber={getNumber}  flg={flag}></Guessnumber>
+    </div>
+  );
+}
+
+export default App;
---
+++ Number Guessing Game/number_guessing_game/src/components/Guessnumber.js
@@ -0,0 +1,20 @@
+function Guessnumber(prop) {
+  function submitHandler(event){
+    event.preventDefault()
+    prop.enteredNumber(event.target[0].value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler} >
+      <input type="number" />
+      <button  type="submit">Check</button>
+        {/* {console.log(prop.flag)} */}
+        { prop.flag ? <p>Correct</p> : <p>Incorrect</p>}
+       </form> 
+    </div>
+  );
+}
+
+
+export default Guessnumber;
